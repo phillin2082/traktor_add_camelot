1a9e2423b08007a6fb55635d80a709d01317d8d8
phillipws2 <phillipws2@phillipws2>
2018-08-29 16:59:03 -0700

    added comments and README file

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# traktor_add_camelot
+# Product Name
+
+![](camelot_wheel.jpg) ![](harmonic_wheel.png)
+
+## Requirements
+
+None! Except Traktor of course.
+
+## Installation
+
+Download the exe the file to your computer and run.
+
+## Usage example
+
+traktor_add_camelot.exe
+
+## Development setup
+
+TBD
+
+## Release History
+
+* 0.0.1
+    * Work in progress
+
+## Contributing
+
+TBD
--- traktor_add_camelot.py
+++ traktor_add_camelot.py
@@ -3,30 +3,22 @@ from shutil import copy2
 from time import strftime
 from data import CamelotWheel
 
-#for development, comment out at build
-#import sys
-#cur_dir = r'E:\scripts2-git\python\projects\dev\traktor_add_camelot_package'
-#sys.path.append(cur_dir)
-
-nml_fullpath = input("Fullpath of collection.nml file\n")
-
 def backup_nml(nml):
-    """Input fullpath to file"""
+    """Input is full path to file. Backs up file appending timestamp"""
     timestr = strftime("%Y%m%d-%H%M%S")
     src = nml
     dst = nml + timestr
     copy2(src,dst)    
     print("File is backed up to " + dst)
-#%%
-#datafile = '/data/allscripts-git/python/projects/dev/traktor-add-camelot/collection-mod.nml'
-#datafile = r'E:\scripts2-git\python\projects\dev\traktor_add_camelot_package\collection-mod2.nml'
+
+nml_fullpath = input("Fullpath of collection.nml file\n")
+
 datafile = nml_fullpath
 tree = et.parse(datafile)
 root = tree.getroot()
 camelot_wheel = CamelotWheel.camelot_wheel()
 backup_nml(nml_fullpath)
 
-#%%
 print("Writing...")
 entries = list(root.iter('ENTRY'))
 for entry in entries:
@@ -52,10 +44,3 @@ for entry in entries:
 tree.write(datafile)      
 print("Success!")
 input("Press Enter to close out this window.")
-
-
-#%%
-#mndr feed me diamonds, 11a, 18
-#entries[0].find('LOCATION').attrib['FILE']
-#entries[0].find('INFO').attrib['KEY']
-#entries[0].find('MUSICAL_KEY').attrib['VALUE'] = '19'

2b20de824f53e04bd2a2626396765a02b0574720
rhel73-ws01 <rhel73-ws01>
2019-01-30 00:25:44 -0800

    traktor stuff

--- README.md
+++ README.md
@@ -1,18 +1,34 @@
-# Product Name
+# Add Camelot keys to Native Instruments Traktor 2
 
-![](camelot_wheel.jpg) ![](harmonic_wheel.png)
+Using Traktors key analysis with Mixed In Key's Camelot System, this tool writes the Camelot Key to the "Key Text" column. 
+
+![](docs/camelot_wheel.jpg) ![](docs/harmonic_wheel.png)
 
 ## Requirements
 
-None! Except Traktor of course.
+Windows 7/10 and Traktor 2. Will come out with a Mac version soon.
+
+If you havent' done so already, Analyze your tracks in Traktor.
+
+Also, set the musical key options, go to Preferences - Analyze Options and set
+Displayed in Traktor = Musical
+Written to File Tags = Key Text
+
+![](docs/key_settings.png)
 
 ## Installation
 
-Download the exe the file to your computer and run.
+Download the tool to your computer and run it by double clicking.
 
-## Usage example
+## How to use
 
-traktor_add_camelot.exe
+1. Run the tool, it will prompt for the full path of your collecton.nml file. See below screenshot if you don't know where the collection.nml file is located.
+2. The tool will create a backup to the same folder location with the timestamp appended (ex. collection.nml180904-030205)
+3. The tool will replace the "Key Text" field with the Camelot Key
+4. If something goes wrong, delete collection.nml, and restore from backup by renaming the backup file to collection.nml.
+
+How to find your collection.nml file
+![](docs/filepath_nml.png)
 
 ## Development setup
 
--- traktor_add_camelot.py
+++ traktor_add_camelot.py
@@ -1,7 +1,38 @@
-import xml.etree.ElementTree as et
+#import xml.etree.ElementTree as et
+from lxml import etree
 from shutil import copy2
 from time import strftime
-from data import CamelotWheel
+#from data import CamelotWheel
+
+def camelot_wheel():
+    camelot_wheel = [
+        {'traktor_key': 0, 'camelot_key': '08B'},
+        {'traktor_key': 1, 'camelot_key': '03B'},
+        {'traktor_key': 2, 'camelot_key': '10B'},
+        {'traktor_key': 3, 'camelot_key': '05B'},
+        {'traktor_key': 4, 'camelot_key': '12B'},
+        {'traktor_key': 5, 'camelot_key': '07B'},
+        {'traktor_key': 6, 'camelot_key': '02B'},
+        {'traktor_key': 7, 'camelot_key': '09B'},
+        {'traktor_key': 8, 'camelot_key': '04B'},
+        {'traktor_key': 9, 'camelot_key': '11B'},
+        {'traktor_key': 10, 'camelot_key': '06B'},
+        {'traktor_key': 11, 'camelot_key': '01B'},
+        {'traktor_key': 12, 'camelot_key': '05A'},
+        {'traktor_key': 13, 'camelot_key': '12A'},
+        {'traktor_key': 14, 'camelot_key': '07A'},
+        {'traktor_key': 15, 'camelot_key': '02A'},
+        {'traktor_key': 16, 'camelot_key': '09A'},
+        {'traktor_key': 17, 'camelot_key': '04A'},
+        {'traktor_key': 18, 'camelot_key': '11A'},
+        {'traktor_key': 19, 'camelot_key': '06A'},
+        {'traktor_key': 20, 'camelot_key': '01A'},
+        {'traktor_key': 21, 'camelot_key': '08A'},
+        {'traktor_key': 22, 'camelot_key': '03A'},
+        {'traktor_key': 23, 'camelot_key': '10A'},
+    ]
+    return (camelot_wheel)
+
 
 def backup_nml(nml):
     """Input is full path to file. Backs up file appending timestamp"""
@@ -11,18 +42,20 @@ def backup_nml(nml):
     copy2(src,dst)    
     print("File is backed up to " + dst)
 
-nml_fullpath = input("Fullpath of collection.nml file\n")
+#nml_fullpath = input("Fullpath of collection.nml file\n")
+
+nml_fullpath = '/data/allscripts-git/python/projects/dev/traktor_add_camelot_package/collection.nml'
 
 datafile = nml_fullpath
-tree = et.parse(datafile)
+tree = etree.parse(datafile, etree.XMLParser(recover=True))
 root = tree.getroot()
-camelot_wheel = CamelotWheel.camelot_wheel()
+camelot_wheel = camelot_wheel()
 backup_nml(nml_fullpath)
 
 print("Writing...")
 entries = list(root.iter('ENTRY'))
 for entry in entries:
-    #print(entry.tag, entry.attrib)
+    print(entry.tag, entry.attrib)
     filename = ''
     traktor_key = ''
     key = ''
@@ -39,7 +72,7 @@ for entry in entries:
         for camelot_spoke in camelot_wheel:
             if int(traktor_key) == int(camelot_spoke['traktor_key']):
                 entry.find('INFO').attrib['KEY'] = camelot_spoke['camelot_key']
-                #print(camelot_spoke['traktor_key'], camelot_spoke['camelot_key'])
+                print(camelot_spoke['traktor_key'], camelot_spoke['camelot_key'])
        
 tree.write(datafile)      
 print("Success!")
